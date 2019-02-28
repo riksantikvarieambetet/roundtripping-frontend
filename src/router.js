e72e0b7ce0238f6a7eb40f60177d2b88bd37dd04
Paweł Marynowski <pawel.marynowski@outlook.com>
2019-03-01 00:58:52 +0100

    add login bootstrap

--- src/router.js
+++ src/router.js
@@ -1,25 +1,56 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
 
-Vue.use(Router)
+import Home from "./views/Home.vue";
+import Login from "./views/Login.vue";
 
-export default new Router({
-  mode: 'history',
+Vue.use(Router);
+
+const router = new Router({
+  mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: "/",
+      name: "home",
+      component: Home,
+      meta: { requiresAuth: true }
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: "/login",
+      name: "login",
+      component: Login,
+      meta: { guest: true }
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/About.vue"),
+      meta: { requiresAuth: true }
     }
   ]
-})
+});
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem("jwt");
+  const isGuest = to.matched.some(record => record.meta.guest);
+  const isAuth = to.matched.some(record => record.meta.requiresAuth);
+
+  if (isAuth && token === null) {
+    next({
+      path: "/login",
+      params: { nextUrl: to.fullPath }
+    });
+  } else if (isGuest) {
+    if (token == null) {
+      next();
+    } else {
+      next({ name: "home" });
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
