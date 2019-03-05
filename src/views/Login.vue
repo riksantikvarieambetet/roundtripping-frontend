820f273a87130aa0e4c086bcc4117390c13d05d4
Paweł Marynowski <pawel.marynowski@outlook.com>
2019-03-06 00:03:57 +0100

    some login adjustments

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,21 +1,20 @@
 <template>
-  <div>
-    <h4>Login</h4>
-    <form>
-      <label for="email">E-Mail Address</label>
-      <div>
-        <input id="email" type="email" v-model="email" required autofocus>
-      </div>
-      <div>
-        <label for="password">Password</label>
-        <div>
-          <input id="password" type="password" v-model="password" required>
+  <div class="jumbotron">
+    <div class="container">
+      <form>
+        <div class="form-group">
+          <label for="email">E-Mail Address</label>
+          <input id="email" class="form-control" type="email" v-model="email" required autofocus>
         </div>
-      </div>
-      <div>
-        <button type="submit" @click="handleSubmit">Login</button>
-      </div>
-    </form>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input id="password" class="form-control" type="password" v-model="password" required>
+        </div>
+        <div>
+          <button class="btn btn-primary" type="submit" @click="handleSubmit">Login</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
