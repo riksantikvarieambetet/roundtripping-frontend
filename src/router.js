fe9b6f4706cd26b28ef173276201b8cc50ea6f01
Abbe98 <albin.post@gmail.com>
2019-04-21 11:18:48 +0200

    remove login stuff

--- src/router.js
+++ src/router.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import Router from "vue-router";
 
 import Home from "./views/Home.vue";
-import Login from "./views/Login.vue";
 
 Vue.use(Router);
 
@@ -14,64 +13,32 @@ const router = new Router({
       path: "/",
       name: "home",
       component: Home,
-      meta: { requiresAuth: true }
-    },
-    {
-      path: "/login",
-      name: "login",
-      component: Login,
-      meta: { guest: true }
     },
     {
       path: "/about",
       name: "about",
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue"),
-      meta: { requiresAuth: true }
     },
     {
       path: "/institution/:id",
       name: "institution",
       component: () =>
         import(/* webpackChunkName: "institution" */ "./views/Institution.vue"),
-      meta: { requiresAuth: true }
     },
     {
       path: "/collection/:institutionId/:id",
       name: "collection",
       component: () =>
         import(/* webpackChunkName: "collection" */ "./views/Collection.vue"),
-      meta: { requiresAuth: true }
     },
     {
       path: "/translations/:id",
       name: "translations",
       component: () =>
         import(/* webpackChunkName: "translation" */ "./views/Translation.vue"),
-      meta: { requiresAuth: true }
     }
   ]
-});
-
-router.beforeEach((to, from, next) => {
-  const token = localStorage.getItem("jwt");
-  const isGuest = to.matched.some(record => record.meta.guest);
-  const isAuth = to.matched.some(record => record.meta.requiresAuth);
-
-  if (isAuth && token === null) {
-    next({
-      path: "/login",
-      params: { nextUrl: to.fullPath }
-    });
-  } else if (isGuest) {
-    if (token == null) {
-      next();
-    } else {
-      next({ name: "home" });
-    }
-  } else {
-    next();
-  }
 });
 
 export default router;
